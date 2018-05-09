e5738de285015a8963b39991e024167e1578a4a6
Hiram <hriamtan@live.com>
2018-05-09 14:47:42 +0800

    add support dll

--- unity/Assets/GreeterClient/Program.cs
+++ unity/Assets/GreeterClient/Program.cs
@@ -29,6 +29,7 @@ namespace GreeterClient
 
             var reply = client.SayHello(new HelloRequest { Name = user });
             Console.WriteLine("Greeting: " + reply.Message);
+           UnityEngine.Debug.Log("Greeting: " + reply.Message);
 
             channel.ShutdownAsync().Wait();
             Console.WriteLine("Press any key to exit...");
