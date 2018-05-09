a3764ce1728e588f8ecdd7927cdfb5510dd76142
Hiram <hriamtan@live.com>
2018-05-09 14:57:01 +0800

    add example

---
+++ unity/Assets/Example.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        GreeterServer.Program.Main(null);
+      
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 100, 40), "send"))
+        {
+            GreeterClient.Program.Main(null);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
